5f686e02340b9f6ae15f9308bbce688ea5b35ce8
Gd Sousa <gunnark6527@gmail.com>
2024-11-15 22:25:45 -0300

    att

--- Program.cs
+++ Program.cs
@@ -1,13 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Infraestrutura.Db;
+using MinimalApi.DTOs;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<DbContexto>(options =>{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.MapPost("/login", (MinimalApi.DTOs.LoginDTO loginDTO)=>{
+
+app.MapPost("/login", (LoginDTO loginDTO)=>{
     if(loginDTO.Email == "adm@teste.com" && loginDTO.Senha =="12345"){
         return Results.Ok("Logado com sucesso!");
     }else{
         return Results.Unauthorized();
     }
 });
-
+ 
 app.Run();
